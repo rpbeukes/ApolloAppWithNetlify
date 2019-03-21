888bfa6388e632dd408aeb6b41393ccfd6198186
Ruan Beukes <ruan.beukes@readify.net>
2019-03-21 09:59:18 +0800

    add proxy for lambda local development

---
+++ src/setupProxy.ts
@@ -0,0 +1,15 @@
+/*
+netlify-lambda use this proxy to run things locally.
+https://www.netlify.com/docs/functions/#tools-for-building-javascript-functions
+*/
+
+import proxy from 'http-proxy-middleware'
+
+module.exports = function(app: any) {
+  app.use(proxy('/.netlify/functions/', { 
+    target: 'http://localhost:9000/',
+    "pathRewrite": {
+      "^/\\.netlify/functions": ""
+    }
+  }));
+};
